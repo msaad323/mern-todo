dbde614b2f89644116170abb6dca437534517e9f
Mohd Saad <mohd.saad@mobiuso.com>
2025-05-16 01:53:50 +0530

    Enable CORS url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mern-todo-kohl-pi.vercel.app/',
   credentials: true, // Allow cookies to be sent
 }));
 
